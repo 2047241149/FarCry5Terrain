c2a7a16ca61c36450f2511eec12bb91036104d49
XiaoMaoGou <2047241149@qq.com>
2023-08-22 02:04:57 +0800

    implement landscape vertex shader gpu sample pos and normal(TextureRGBA32f: r: height gba:normal)

--- Assets/Material/LandscapeShader.shader
+++ Assets/Material/LandscapeShader.shader
@@ -4,6 +4,7 @@ Shader "Landscape/RenderLandscape"
     {
         _HeightTex ("Texture", 2D) = "white" {}
         _HeightScale ("HeightScale", float) = 1.0
+        _bPureWhite("bPureWhite", int) = 0
     }
     SubShader
     {
@@ -31,18 +32,23 @@ Shader "Landscape/RenderLandscape"
                 float2 uv : TEXCOORD0;
                 UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
+                float3 worldNormal : NORMAL; 
             };
 
             sampler2D _HeightTex;
             float _HeightScale;
+            int _bPureWhite;
 
             v2f vert (appdata v)
             {
                 v2f o;
-                float heightSampleValue = tex2Dlod(_HeightTex, float4(v.uv, 0, 0)).r;
+                float4 heightNormal = tex2Dlod(_HeightTex, float4(v.uv, 0, 0)).rgba;
+                float heightSampleValue = heightNormal.r;
+                float3 normal = heightNormal.gba;
                 float height = heightSampleValue * _HeightScale;
                 float4 landscapeVertex = float4(v.vertex.x, height, v.vertex.z, 1.0f);
                 o.vertex = UnityObjectToClipPos(landscapeVertex);
+                o.worldNormal = UnityObjectToWorldNormal(normal);
                 o.uv = v.uv;
                 UNITY_TRANSFER_FOG(o,o.vertex);
                 return o;
@@ -50,8 +56,14 @@ Shader "Landscape/RenderLandscape"
 
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
                 fixed4 col = fixed4(1.0f, 1.0f, 1.0f, 1.0f);
+                if(_bPureWhite == 0)
+                {
+                    // sample the texture
+                    float3 normalizedLightDir = normalize(_WorldSpaceLightPos0.xyz);
+                    fixed halfLambert = dot(i.worldNormal, normalizedLightDir) * 0.5 + 0.5;
+                    col = fixed4(halfLambert, halfLambert, halfLambert, 1.0f);
+                }
 
                 return col;
             }
--- Assets/Script/Landscape.cs
+++ Assets/Script/Landscape.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Landscape : MonoBehaviour
 {
@@ -14,20 +16,20 @@ public class Landscape : MonoBehaviour
     private int halfHeightResolution;
     private int realWidthResolution;
     private int realHeightResolution;
+    private MeshRenderer meshRender;
     
     public Texture2D heightMap;
-    public float size = 1.0f;
+    public float cellSize = 1.0f;
     public float heightScale = 1.0f;
     public int widthResolution = 1024;
     public int heightResolution = 1024;
+    public bool bPureWhite = false;
     public Material material;
-    
-    
+    private Texture2D calHeightMap;
     
     // Start is called before the first frame update
     void Start()
     {
-        
         Mesh mesh = new Mesh();
         if (!mesh)
         {
@@ -40,13 +42,13 @@ public class Landscape : MonoBehaviour
             return;
         }
 
-        MeshRenderer meshRender = GetComponent<MeshRenderer>();
+        meshRender = GetComponent<MeshRenderer>();
         if (!meshRender)
         {
             return;
         }
-        
-        
+
+        RecalculateHeightsAndNormals();
         halfWidthResolution = widthResolution / 2;
         halfHeightResolution = heightResolution / 2;
         realWidthResolution = halfWidthResolution * 2;
@@ -60,23 +62,25 @@ public class Landscape : MonoBehaviour
         {
             for (int x = -halfWidthResolution; x <= halfWidthResolution; x++)
             {
-                vertices[index] = new Vector3(x * size, 0, y * size);
+                vertices[index] = new Vector3(x * cellSize, 0, y * cellSize);
                 uvs[index] = new Vector2((float)(x + halfWidthResolution) / (float)realWidthResolution,
                     (float)(y + halfHeightResolution) / (float)realHeightResolution);
                 index++;
             }
         }
-
-
-
+        
+        
         mesh.vertices = vertices;
         mesh.uv = uvs;
+        
+        // for UInt16, max index is 65535, if more than use UInt32
+        mesh.indexFormat = IndexFormat.UInt32;
         
         //calculate index
         int triangleNum = realWidthResolution *  realHeightResolution * 2;
         int[] indices = new int[triangleNum * 3];
         index = 0;
-        for (int y = 0; y < realHeightResolution; y++)
+        for ( int y = 0; y < realHeightResolution; y++)
         {
             for (int x = 0; x < realWidthResolution; x++)
             {
@@ -116,13 +120,75 @@ public class Landscape : MonoBehaviour
         mesh.RecalculateBounds();
         meshFilter.mesh = mesh;
         meshRender.material = material;
-        meshRender.material.SetTexture("_HeightTex", heightMap);
+        meshRender.material.SetTexture("_HeightTex", calHeightMap);
         meshRender.material.SetFloat("_HeightScale", heightScale);
+    }
+    
+
+    void RecalculateHeightsAndNormals()
+    {
+        if (!heightMap)
+            return;
+        
+        Color[] heightData = heightMap.GetPixels(0);
+        if (heightData.Length != heightMap.width * heightMap.height)
+            return;
+        
+        //TODO: use RGBA8 to replace RGBA16
+        calHeightMap = new Texture2D(heightMap.width, heightMap.height, TextureFormat.ARGB32, false);
+        Vector3[] vertexNormals = new Vector3[heightMap.width * heightMap.height];
+        for (int y = 0; y < heightMap.height - 1; y++)
+        {
+            for (int x = 0; x < heightMap.width - 1; x++)
+            {
+                // height
+                int index00 = y * heightMap.width + x;
+                float height00 = heightData[index00].r * heightScale;
+                Vector3 vertex00 = new Vector3(x * cellSize, height00, y * cellSize);
+                
+                int index01 = (y + 1) * heightMap.width + x;
+                float height01 =  heightData[index01].r * heightScale;
+                Vector3 vertex01 = new Vector3(x * cellSize, height01, (y + 1) * cellSize);
+                
+                int index10 = y * heightMap.width + x + 1;
+                float height10 =  heightData[index10].r * heightScale;
+                Vector3 vertex10 = new Vector3((x + 1) * cellSize, height10, y * cellSize);
+                
+                int index11 = (y + 1) * heightMap.width + x + 1;
+                float height11 =  heightData[index11].r * heightScale;
+                Vector3 vertex11 = new Vector3((x + 1) * cellSize, height11, (y + 1) * cellSize);
+
+                Vector3 faceNormal1 = math.cross(vertex01 - vertex00, vertex11 - vertex01);
+                Vector3 faceNormal2 = math.cross(vertex00 - vertex10, vertex11 - vertex00);
+                
+                vertexNormals[index01] += faceNormal1;
+                vertexNormals[index10] += faceNormal2;
+                vertexNormals[index00] += faceNormal1 + faceNormal2;
+                vertexNormals[index11] += faceNormal1 + faceNormal2;
+            }
+        }
+
+        for (int y = 0; y < heightMap.height; y++)
+        {
+            for (int x = 0; x < heightMap.width; x++)
+            {
+                int index = y * heightMap.width + x;
+                Vector3 vertexNormal = vertexNormals[index].normalized;
+                float height = heightData[index].r;
+                heightData[index] = new Color(height, vertexNormal.x, vertexNormal.y, vertexNormal.z);
+            }
+        }
+        
+        calHeightMap.SetPixels(heightData, 0);
+        calHeightMap.Apply();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (meshRender)
+        {
+            meshRender.material.SetInt("_bPureWhite", bPureWhite ? 1 : 0);
+        }
     }
 }
